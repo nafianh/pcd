d572b789a1c8f325708ab63b6a6b2917ac7c9ca8
nafianh <nafianisaulh@gmail.com>
2017-03-13 12:49:31 +0700

    Tugas PCD 2

---
+++ src/gambar/Gambar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gambar;
+
+/**
+ *
+ * @author DELL
+ */
+public class Gambar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
